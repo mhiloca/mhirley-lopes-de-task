1bc5cc4d831d4cbcdc054a1a3d1cc9afd2069d88
Mhirley Lopes <mhiloca@gmail.com>
2024-08-03 10:39:31 -0300

    :art: Refac - Change sf user

--- scripts/local_setup.sh
+++ scripts/local_setup.sh
@@ -1,9 +1,9 @@
-export SF_ACCOUNT='uezpxfr-tt82220'
-export SF_USER="deel"
-export SF_PASSWORD="deel123"
+export SF_ACCOUNT='dpnvsui-fr01571'
+export SF_USER="dbt"
+export SF_PASSWORD="dbtPassword123"
 export SF_ROLE="transform"
 export SF_WAREHOUSE="COMPUTE_WH"
-export SF_DATABASE="NYT_BOOKS"
+export SF_DATABASE="NYT"
 export SF_SCHEMA="RAW"
 export NYT_API_KEY="kYus8i0AFTuoUxgeC06so0zLxhqPcAn8"
 
